680df74eb6b3c1fe75ed10786f68c862c04d35da
pigeon-liu <1057150559@qq.com>
2020-09-10 22:59:46 +0800

    括号匹配

--- Plan/src/Niuke9_7/Solution6.java
+++ Plan/src/Niuke9_7/Solution6.java
@@ -13,7 +13,8 @@ public class Solution6 {
         //将字符串转换为数组
         char[] chars = s.toCharArray();
         Stack<Character> stack = new Stack<>();
-        //stack.add(chars[0]);
+
+        /*stack.add(chars[0]);
         // 遍历数组
         for(int i = 1;i<s.length();i++){
             stack.add(chars[i]);
@@ -23,6 +24,35 @@ public class Solution6 {
                 char ch = stack.peek();
                 if(ch=='{' && chars[i]=='}' ||ch=='(' && chars[i]==')'||ch=='[' && chars[i]==']' ){
                     stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        if(stack.empty()){
+            return true;
+        } else {
+            return false;
+        }*/
+
+
+
+        for(int i = 0;i<s.length();i++){
+            // 遍历数组放入栈中
+            stack.push(chars[i]);
+            //判断如果是右括号，进行出栈，在判断下一个是否与之匹配
+            if(chars[i] == '}' ||chars[i] == ']' ||chars[i] == ')'){
+                stack.pop();
+                if (!stack.empty()){
+                    char ch = stack.peek();
+                    //判断是否匹配
+                    if ((ch == '{' && chars[i] == '}') || (ch == '(' && chars[i] == ')' )|| (ch == '[' && chars[i] == ']')){
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return false;
                 }
             }
         }
@@ -35,7 +65,7 @@ public class Solution6 {
 
     public static void main(String[] args) {
         Solution6 t = new Solution6();
-        String str = "}";
+        String str = "[";
         System.out.println(t.isValid(str));
     }
 }
